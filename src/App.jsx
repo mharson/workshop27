a041be763c4df19730f95b4602f6e2011741fe29
mharson <meganharson@gmail.com>
2024-04-20 17:58:32 -0500

    terminal submission

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import { useState } from "react";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
 
+export default function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <>
+      <Authenticate token={token} setToken={setToken} />
+      <SignUpForm token={token} setToken={setToken} />
+    </>
+  );
+}
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  async function handleSubmit(event) {
+    setToken(result.token); //may need to move
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            username: "some-username",
+            password: "super-secret-999",
+          }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  return (
+    <>
+      <h2> Sign Up!</h2>
+      {error && <p>{error}</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:{" "}
+          <input
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+
+        <button>Submit</button>
+      </form>
+    </>
+  );
+}
